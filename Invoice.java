2b5b7a6145db678ce55f3757078920c3fa5a8840
Niharika-Alaparthi <115446905+Niharika-Alaparthi@users.noreply.github.com>
2022-10-10 11:22:31 +0530

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Invoice{
 	String partnumber;
 	String partdescription;
@@ -43,6 +44,27 @@ class Invoice{
 		double amount=quantity*priceperitem;
 		return amount;
 	}
+}
+
+public class InvoiceTest
+{
+  public static void main(String[] args) 
+  {
+      Scanner sc=new Scanner(System.in);
+      Invoice in=new Invoice();
+      System.out.print("Enter part number :");
+      in.setpartnumber(sc.next());
+      System.out.print("Enter no. of items purchased :");
+      in.setquantity(sc.nextInt());
+      System.out.print("Enter part description :");
+      in.setpartdescription(sc.next());
+      System.out.print("Enter price per item :");
+      in.setpriceperitem(sc.nextDouble());
+      System.out.print(" Item Details \n");
+      System.out.print("Part number :" + in.getpartnumber()+"\n");
+      System.out.print("\nPart description :" + in.getpartdescription()+"\n");
+      System.out.print("\nTotal Billing Amount :" + in.getInvoiceAmount()+"\n");
+  }
 }
 
 
